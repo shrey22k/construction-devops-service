3f70d111dd2c7040235cdd2549897389cd55beae
Shreyas <shrey22k@gmail.com>
2026-03-14 01:42:13 +0530

    Added CORS support

--- ConstructionService/Program.cs
+++ ConstructionService/Program.cs
@@ -1,28 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// services
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
 });
 
 var app = builder.Build();
 
-// middleware
 app.UseCors("AllowAll");
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.MapControllers();
 
